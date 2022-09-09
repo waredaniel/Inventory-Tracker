8343c7208ed2e8eab766e35e23a4e3f2800f9a36
danielware <waredanielb@gmail.com>
2022-09-09 10:05:44 -0700

    Make reusable form.

---
+++ inventory-tracker/src/components/ReusableForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return(
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+         <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <textarea
+          name='Description'
+          placeholder='Description of Soup.' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
